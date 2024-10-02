8a34f3c3b36f8c9e0435874f053d003614bac081
kang-sumin <min001117@gmail.com>
2024-10-02 12:04:26 +0900

    [Level 1-1] 코드 개선 퀴즈 - @Transactional의 이해

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -25,6 +25,16 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    /**
+     * [Level1] 1. 코드 개선 퀴즈 - @Transactional의 이해
+     * TodoService 클래스에  @Transactional(readOnly = true) 애노테이션으로 데이터베이스 작업을 읽기 전용처리해 두었습니다.
+     * 읽기 전용으로 처리하면 데이터베이스의 데이터 무결성 제약을 보호할 수 있고, 데이터 쓰기를 방지하고, 캐시를 효율적으로 사용하여 성능 최적화에 좋습니다.
+     * 하지만 이 트랜잭션 내에서 데이터베이스의 수정(삽입, 업데이트, 삭제) 가 발생하게 되면 예외가 발생하게 됩니다.
+     * 클래스에 @Transactional(readOnly = true)가 설정되어 있을때 수정 작업 메서드를 수행하게 하려면
+     * 해당 메서드에 @Transactional(readOnly = false)를 적용하여 수정 작업이 가능하도록 메서드 레벨에서 상위 설정을 덮어써서 수정 작업을 수행 할 수 있습니다.
+     * 참고로 readOnly 설정의 default 값이 false 여서 생략 가능합니다.
+      */
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
