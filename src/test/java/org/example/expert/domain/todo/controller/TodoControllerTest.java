41072f6c0f89460185cc7792bfd4081da7c19210
kang-sumin <min001117@gmail.com>
2024-10-05 23:06:27 +0900

    [Level 1-4] 테스트 코드 퀴즈 - 컨트롤러 테스트의 이해

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -8,10 +8,13 @@ import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -35,9 +38,9 @@ class TodoControllerTest {
         // given
         long todoId = 1L;
         String title = "title";
-        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER, "sumin Kang");
         User user = User.fromAuthUser(authUser);
-        UserResponse userResponse = new UserResponse(user.getId(), user.getEmail());
+        UserResponse userResponse = new UserResponse(user.getId(), user.getEmail(), user.getNickname());
         TodoResponse response = new TodoResponse(
                 todoId,
                 title,
@@ -68,10 +71,17 @@ class TodoControllerTest {
                 .thenThrow(new InvalidRequestException("Todo not found"));
 
         // then
+        /**
+         * 400 에러가 발생하는데 200 상태코드를 검증하고 있어서 발생한 버그였음
+         * 해당 테스트는 todo가 존재하지 않아 400 상태코드가 잘 발생하는지를 테스트 함으로 기대값을 Bad request가 잘 발생하는지 검증으로 해야함
+         * 404 NOT_FOUND 상태코드여야 하지 않을까 생각했지만, 찾아보니 404는 해당 API URI를 조회하지 못했다는 의미가 강하기 때문에
+         * DB에서 데이터를 찾지 못했을때는 400 BAD_REQUEST가 적절하다고 한다.
+         * 그래서 많은 예외 상황들이 400 BAD_REQUEST 상태코드로 응답한다고 한다. (404는 요청받은 리소스(API URI, 요청 경로)를 찾을 수 없을 때 주로 사용)
+         */
         mockMvc.perform(get("/todos/{todoId}", todoId))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(HttpStatus.OK.name()))
-                .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.name()))
+                .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(jsonPath("$.message").value("Todo not found"));
     }
 }
