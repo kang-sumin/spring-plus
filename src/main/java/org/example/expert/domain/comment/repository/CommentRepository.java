8a495ec2c04a13636e45b20d17fdf44458dda089
kang-sumin <min001117@gmail.com>
2024-10-07 15:32:35 +0900

    [Level 2-7] N+1

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    /**
+     * N+1 문제를 해결하기 위해 FETCH JOIN 방식을 사용하였습니다.
+     * 해당 쿼리에서 Comment 엔티티와 연관된 User엔티티를 FETCH JOIN으로 함께 조회해서 가져오면서 N+1 문제를 해결하였습니다.
+     */
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
